0fb85127ee5a7ce4514255486f9c7bdee6124716
Brahamjot Singh <crownbrahamjot2004@gmail.com>
2026-09-01 19:41:45 +0530

    feat: add site verification meta tag to document head

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en" data-scroll-behavior="smooth">
       <Head>
+        <meta
+          name="impact-site-verification"
+          content="632eb0d1-2261-4f54-82ad-44402c23b605"
+        />
         <link rel="icon" href="/favicon.png" type="image/png" />
       </Head>
       <body>
